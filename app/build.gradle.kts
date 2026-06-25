9a3475390e34ef6d8b15417effeb4477d00e17be
Faizan-dev <ahmedfaizan931star@gmail.com>
2026-06-25 09:57:41 +0500

    Set Kotlin JVM target to version 17
    Added Kotlin options for JVM target version.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,9 +49,12 @@ android {
         }
     }
 
-    compileOptions {
+compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
     }
 
     buildFeatures { compose = true }
